623569220edd3f2a9316e8fd8b53b87573a4b9fc
Dr. Cornelius Eichner <ceichner@ramones.cbs.mpg.de>
2021-02-16 15:25:42 +0100

    Remove old processing script filenames

--- 2_make_nii.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Load Local Variables
-source ./SET_VARIABLES.sh
-
-# Convert Bruker Data to Nifti
-${SOFTWARE}/bru2/Bru2 -a -p -z -v ${BRUKER_RAW_DIR}/subject
-mv ${BRUKER_RAW_DIR}/*.nii.gz ${NII_RAW_DIR}
-
