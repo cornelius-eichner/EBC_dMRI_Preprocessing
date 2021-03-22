e77bdb72a58b35d0b1f7ddfbe19fc6aae5379646
cornelius-eichner <ceichner@orinoko.cbs.mpg.de>
2021-03-22 17:12:57 +0100

    Bugfix

--- 10_diff_run_bedpost.sh
+++ 10_diff_run_bedpost.sh
@@ -12,7 +12,7 @@ echo "Copy Data to Bedpost Folder"
 
 cp ${DIFF_DATA_DIR}/data_debias_denoise_detrend_eddy.nii.gz ${DIFF_DATA_BEDPOSTX_DIR}/data.nii.gz
 cp ${DIFF_DATA_DIR}/mask.nii.gz 							${DIFF_DATA_BEDPOSTX_DIR}/nodif_brain_mask.nii.gz
-cp ${DIFF_DATA_DIR}/data.bvals 								${DIFF_DATA_BEDPOSTX_DIR}/bvals
+cp ${DIFF_DATA_DIR}/data.bval 								${DIFF_DATA_BEDPOSTX_DIR}/bvals
 cp ${EDDY_DIR}/*bvecs		 								${DIFF_DATA_BEDPOSTX_DIR}/bvecs
 
 
