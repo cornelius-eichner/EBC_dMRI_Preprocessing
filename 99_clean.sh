1a4a64a61723328648c4f0b7b472a65cfe32a10c
Cornelius <cornelius.eichner@gmail.com>
2021-05-11 15:27:22 +0200

    move cleaning script to the far end and include number of N4 for flash registration

---
+++ 99_clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Following FSL and CUDA Version need to be active
+# CUDA --version 8.0
+# FSL --version 5.0.11
+
+
+# Load Local Variables
+source ./SET_VARIABLES.sh
+
+echo "Removing intermedidate calculation folders"
+
+rm -rf \
+	${DIFF_DATA_N4_DIR} \
+	${EDDY_DIR} \
+	${EDDY_FIELDS_DIR} \
+	${EDDY_FIELDS_REL_DIR} \
+	${EDDY_FIELDS_JAC_DIR} \
+	${NII_RAW_DIR} \
+	${REORIENT_DIR} \
+	${SPLIT_DIR} \
+	${SPLIT_WARPED_DIR} \
+
+
+echo $0 " Done" 
